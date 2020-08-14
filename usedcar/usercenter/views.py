8080f2933a555632716a9e329649058c26fb4a94
Jocelyn6007 <jocelyn6007@126.com>
2020-08-14 13:42:08 +0800

    202008141342

--- usedcar/usercenter/views.py
+++ usedcar/usercenter/views.py
@@ -1,4 +1,5 @@
 #http:127.0.0.1:8000/beta/users/<username>/
+import hashlib
 import json
 
 from django.views import View
@@ -23,13 +24,13 @@ class UserCenterViews(View):
         pass
 
     #由于分步式路由最后是<str:username>,所以视图函数需要添加同名参数.
-    def post(self,request,username):
+    def post(self,request,username=None):
         #前端用post方式发请求, 后端从请求体中取数据.
         post_data=request.body
         post_data=json.loads(post_data)
         #print(post_data)#{'username': 'momo', 'tag': 'sell'}
 
-        #处理前端 我的卖车记录标签
+        #---------处理前端 我的卖车记录标签-----
         if post_data["tag"]=="sell":
             #从数据库取出对应username的车辆信息:<QuerySet [{'cars__car_model': '宝骏 510 2019款 1.5 自动 CVT乐享型 国VI'...}]
             ret=User.objects.filter(username=post_data["username"]).values("cars__car_model","cars__brand","cars__car_licence","cars__origin_price","cars__selling_price","cars__mileage","cars__purchase_time","cars__emissions","cars__is_checked","cars__is_active","cars__updated_time","cars__created_time")
@@ -40,31 +41,56 @@ class UserCenterViews(View):
             else:
                 print("没有卖车记录")
                 return JsonResponse({"code":10601,"error":"no data"})
-        elif post_data["tag"]=="modify_password":
-            #分两步走,1 校验旧密码
 
-            #2 存入新密码- 参考注册的密码散列存储和登录的签发token.
+        #--------处理前端 修改密码------------
+        # 分两步走,1 校验旧密码
+        elif post_data["tag"]=="check_password":
+            username=post_data["username"]
+            old_pwd=post_data["old_pwd"]
+            #1. 校验用户名
+            try:
+                user = User.objects.get(username=username)
+            except Exception as e:
+                print("--get user error is", e)
+                result = {
+                    "code": 10602,
+                    "error": "The username or password is error"
+                }
+                return JsonResponse(result)
+            #2.校验密码
+            m = hashlib.md5()
+            m.update(old_pwd.encode())
+            if m.hexdigest() != user.password:
+                result = {
+                    "code": 10603,
+                    "error": "The username or password is error!"
+                }
+                return JsonResponse(result)
+            #3. 返回校验结果
+            print(11111111,m.hexdigest)
+            return JsonResponse({"code":200,"data":"The username or password is correct"})
+        #2 存入新密码- 参考注册的密码散列存储和登录的签发token.
             pass
 
 
 
     #修改密码：
     #添加校验登录状态的装饰器
-    @method_decorator(logging_check)
-    def put(self, request, username=None):
-        json_str = request.body
-        json_obj = json.loads(json_str)
-        sign = json_obj["sign"]
-        info = json_obj["info"]
-        nickname = json_obj["nickname"]
-
-        user = UserProfile.objects.get(username=username)
-        user.sign = sign
-        user.info = info
-        user.nickname = nickname
-
-        user.save()
-        return JsonResponse({"code": 200, "username": username})
+    # @method_decorator(logging_check)
+    # def put(self, request, username=None):
+    #     json_str = request.body
+    #     json_obj = json.loads(json_str)
+    #     sign = json_obj["sign"]
+    #     info = json_obj["info"]
+    #     nickname = json_obj["nickname"]
+    #
+    #     user = UserProfile.objects.get(username=username)
+    #     user.sign = sign
+    #     user.info = info
+    #     user.nickname = nickname
+    #
+    #     user.save()
+    #     return JsonResponse({"code": 200, "username": username})
         #与数据库核对密码，前端异步显示密码是否输入正确。
         #接收2次新密码，核对2次新密码是否一致
         #如一致，修改数据库密码
